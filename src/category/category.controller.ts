b9e85b1de8f23c70bb79f75176792d506acf98ea
Luis Fernando Flores dos Santos <luis.santos@govbr.com.br>
2020-06-21 22:06:03 -0300

    initial struct

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,22 +1,37 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { get } from 'http';
+import { Controller, Get, Post, Req, Render } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Category } from 'src/category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller('categories')
 export class CategoryController {
 
   constructor(
+    @InjectRepository(Category)
     private categoryRepo: Repository<Category>
-  ){}
+  ) { }
 
   @Get()
-  index(): {
-    return 'Categorias'
+  index(): Promise<Category[]> {
+    return this.categoryRepo.find()
   }
 
-@Get()
-category_create(){
+  @Get('list')
+    @Render('category_list')
+  async category_list(): Promise<Category[]> {
+    const categories = await this.categoryRepo.find()
+    console.log(categories)
+    return {layout: false, categories}
+    }
 
-  }
+@Get('create')
+  category_create(): Promise<Category>{
+  const category = this.categoryRepo.create({ name: 'category teste' })
+  return this.categoryRepo.save(category)
+}
+  // @Post()
+  // async store(@Req() request: Request) {
+  //  const category = await this.categoryRepo.create(request.body as any)
+  //   return this.categoryRepo.save(category)
+  // }
 }
